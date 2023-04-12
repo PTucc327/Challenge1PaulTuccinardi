088d59d86739a59231c5bcaa9febaac5c34620e2
PTucc327 <47836074+PTucc327@users.noreply.github.com>
2023-04-12 10:19:51 -0400

    Add files via upload

---
+++ Challenge1.jsx
@@ -0,0 +1,12 @@
+<>
+  <title>Challenge 1</title>
+  <meta charSet="UTF-8" />
+  <meta name="author" content="Paul Tuccinardi" />
+  <link rel="stylesheet" href="style.css" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <br />
+  <h1> Lightfall Super League Challenge 1</h1>
+  <h2> By: Paul Tuccinardi</h2>
+  <button id="button">Counter</button>
+  <p id="click-counter">Button clicked: </p>
+</>
